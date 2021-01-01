ecd08621aae7bb872aa978874412e8ffc1cbe648
Abdulhadi Abdullah Mohamed <aa6mohamed@uwaterloo.ca>
2021-01-01 17:39:25 -0500

    first simple ray tracer

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+using namespace std;
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    // linearly interpolation from -1 - 1 range to 0 - 1 range
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    // blending colors from blue to white
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
+
+/*
+    Ray tracing goal: Use information about the ray passing through each of the viewport pixels from the origin (eye location) to determine the pixel colors
+*/
+
+int main() {
+    // Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+    
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    // Render
+
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = image_height-1; j >= 0; --j) {
+        cerr << "\rScanlines remaining: " << j << ' ' << flush;
+        for (int i = 0; i < image_width; ++i) {
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(cout, pixel_color);
+            // color pixel_color(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
+            // write_color(cout, pixel_color);
+        }
+    }
+    cerr << "\nDone.\n";
+    return 0;
+}
